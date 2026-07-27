b24ec90eb49d8c6c35ed00bcb00181dcd57ac137
Dingwen Wang <wangdingwen32@gmail.com>
2026-07-27 17:57:07 +0800

    Bump compile/target SDK to 37
    Updates app/build.gradle.kts to use SDK 37 for both compileSdk and targetSdk.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,12 @@ plugins {
 
 android {
     namespace = "net.extrawdw.apps.miuisucks.powerkeeper"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "net.extrawdw.apps.miuisucks.powerkeeper"
         minSdk = 30
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "0.1.0"
 
